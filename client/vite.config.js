5655b15df800d3ed1d064eb33c951af4d826c5c2
Sameer <shivgansameer18@gmail.com>
2025-07-28 12:46:45 +0530

    changed redirect to htaccess

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: "public/_redirects",
+          src: "public/.htaccess",
           dest: ".", // will go into dist/
         },
       ],
